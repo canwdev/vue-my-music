deddc281f2208ba3523e215faa64229d0aae4331
canwdev <c2097@live.com>
2019-01-07 22:45:59 +0800

    init project, add nav, router

---
+++ src/views/rank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view-search">
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "../assets/css/theme.styl"
+</style>
